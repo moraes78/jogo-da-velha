f8c178e1ff887859f4096d9ae4218e1527dadafc
Joel Marcio <joelm@bol.com.br>
2011-10-15 14:04:39 -0300

    Update src/JogoDaVelha.java

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -21,8 +21,8 @@ public class JogoDaVelha {
 
 	public static String[][] inicializar() {
 		String tabuleiroEsperado[][] = new String[][] { { "", "", "" },
-														{ "", "", "" }, 
-														{ "", "", "" } };
+								{ "", "", "" }, 
+								{ "", "", "" } };
 		return tabuleiroEsperado;
 	}
 
@@ -38,7 +38,7 @@ public class JogoDaVelha {
 			int i, int j) throws JogoDaVelhaException {
 
 		if (i >= tabuleiro.length || j >= tabuleiro.length) {
-			throw new JogoDaVelhaException("pocisão fora");
+			throw new JogoDaVelhaException("pocisï¿½o fora");
 		}
 
 	}
@@ -53,86 +53,86 @@ public class JogoDaVelha {
 	}
 
 	public static boolean existeGanhador(String[][] tabuleiro, String jogador) {
-		  	   if ((tabuleiro[0][0].equals("X")) 
-				&& (tabuleiro[0][1].equals("X"))
-				&& (tabuleiro[0][2].equals("X"))) {
+		  	  if ((tabuleiro[0][0].equals("X")) 
+			   && (tabuleiro[0][1].equals("X"))
+			   && (tabuleiro[0][2].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[1][0].equals("X"))
-				&& (tabuleiro[1][1].equals("X"))
-				&& (tabuleiro[1][2].equals("X"))) {
+		        && (tabuleiro[1][1].equals("X"))
+			&& (tabuleiro[1][2].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[2][0].equals("X"))
-				&& (tabuleiro[2][1].equals("X"))
-				&& (tabuleiro[2][2].equals("X"))) {
+			&& (tabuleiro[2][1].equals("X"))
+			&& (tabuleiro[2][2].equals("X"))) {
 			return true;
 			
 		} else if ((tabuleiro[0][0].equals("X"))
-				&& (tabuleiro[1][1].equals("X"))
-				&& (tabuleiro[2][2].equals("X"))) {
+			&& (tabuleiro[1][1].equals("X"))
+			&& (tabuleiro[2][2].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][2].equals("X"))
-				&& (tabuleiro[1][1].equals("X"))
-				&& (tabuleiro[2][0].equals("X"))) {
+			&& (tabuleiro[1][1].equals("X"))
+			&& (tabuleiro[2][0].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][0].equals("O"))
-				&& (tabuleiro[0][1].equals("O"))
-				&& (tabuleiro[0][2].equals("O"))) {
+			&& (tabuleiro[0][1].equals("O"))
+			&& (tabuleiro[0][2].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[1][0].equals("O"))
-				&& (tabuleiro[1][1].equals("O"))
-				&& (tabuleiro[1][2].equals("O"))) {
+			&& (tabuleiro[1][1].equals("O"))
+			&& (tabuleiro[1][2].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[2][0].equals("O"))
-				&& (tabuleiro[2][1].equals("O"))
-				&& (tabuleiro[2][2].equals("O"))) {
+			&& (tabuleiro[2][1].equals("O"))
+			&& (tabuleiro[2][2].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][0].equals("O"))
-				&& (tabuleiro[1][1].equals("O"))
-				&& (tabuleiro[2][2].equals("O"))) {
+			&& (tabuleiro[1][1].equals("O"))
+			&& (tabuleiro[2][2].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][2].equals("O"))
-				&& (tabuleiro[1][1].equals("O"))
-				&& (tabuleiro[2][0].equals("O"))) {
+			&& (tabuleiro[1][1].equals("O"))
+			&& (tabuleiro[2][0].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][0].equals("X"))
-				&& (tabuleiro[1][0].equals("X"))
-				&& (tabuleiro[2][0].equals("X"))) {
+			&& (tabuleiro[1][0].equals("X"))
+			&& (tabuleiro[2][0].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][1].equals("X"))
-				&& (tabuleiro[1][1].equals("X"))
-				&& (tabuleiro[2][1].equals("X"))) {
+			&& (tabuleiro[1][1].equals("X"))
+			&& (tabuleiro[2][1].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][2].equals("X"))
-				&& (tabuleiro[1][2].equals("X"))
-				&& (tabuleiro[2][2].equals("X"))) {
+			&& (tabuleiro[1][2].equals("X"))
+			&& (tabuleiro[2][2].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][0].equals("O"))
-				&& (tabuleiro[1][0].equals("O"))
-				&& (tabuleiro[2][0].equals("O"))) {
+			&& (tabuleiro[1][0].equals("O"))
+			&& (tabuleiro[2][0].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][1].equals("O"))
-				&& (tabuleiro[1][1].equals("O"))
-				&& (tabuleiro[2][1].equals("O"))) {
+			&& (tabuleiro[1][1].equals("O"))
+			&& (tabuleiro[2][1].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][2].equals("O"))
-				&& (tabuleiro[1][2].equals("O"))
-				&& (tabuleiro[2][2].equals("O"))) {
+			&& (tabuleiro[1][2].equals("O"))
+			&& (tabuleiro[2][2].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][0].equals("X"))
-				&& (tabuleiro[1][1].equals("X"))
-				&& (tabuleiro[2][2].equals("X"))) {
+			&& (tabuleiro[1][1].equals("X"))
+			&& (tabuleiro[2][2].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][2].equals("X"))
-				&& (tabuleiro[1][1].equals("X"))
-				&& (tabuleiro[2][0].equals("X"))) {
+			&& (tabuleiro[1][1].equals("X"))
+			&& (tabuleiro[2][0].equals("X"))) {
 			return true;
 		} else if ((tabuleiro[0][0].equals("O"))
-				&& (tabuleiro[1][1].equals("O"))
-				&& (tabuleiro[2][2].equals("O"))) {
+			&& (tabuleiro[1][1].equals("O"))
+			&& (tabuleiro[2][2].equals("O"))) {
 			return true;
 		} else if ((tabuleiro[0][2].equals("O"))
-				&& (tabuleiro[1][1].equals("O"))
-				&& (tabuleiro[2][3].equals("O"))) {
+			&& (tabuleiro[1][1].equals("O"))
+			&& (tabuleiro[2][3].equals("O"))) {
 			return true;
 		}
 		return false;
